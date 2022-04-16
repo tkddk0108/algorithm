0159929dbd219ebd91713abb11a91c23a9a5c21a
tkddk0108 <103806889+tkddk0108@users.noreply.github.com>
2022-04-17 04:36:32 +0900

    Add files via upload

---
+++ exchangesort.cpp
@@ -0,0 +1,24 @@
+//exchangesort 교환정렬 
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	int n=100;
+	int array[100];
+	int temp, i, j;
+	for(i=0; i<100; i++){
+		array[i]=rand()%10000;
+	}
+	for(i=0; i< n-1; i++){
+		for (j=i+1; j<n; j++){
+			if(array[i]>array[j]){
+				temp=array[i];
+				array[i]=array[j];
+				array[j]=temp;
+			}
+		}
+	}
+	for(i=0; i<n; i++){
+		printf("%d ", array[i]);
+	}
+} 
+
